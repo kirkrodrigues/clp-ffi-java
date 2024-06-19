61752822a48eba4dc045bdea1edb07fba46cdb32
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-06-19 04:40:14 -0400

    Install maven as dependency.

--- scripts/install-deps-and-build-native-lib.sh
+++ scripts/install-deps-and-build-native-lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential cmake
+apt-get install -y build-essential cmake maven
 
 mvn --batch-mode validate assembly:single@assemble-lib-dir
